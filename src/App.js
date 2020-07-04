ef07b073d20965defed8d10a9abe74e2531ef265
Prasob kumar kp <prasobkumarkp@gmail.com>
2020-07-04 23:03:35 +0530

    Props and children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <h1>TEST</h1>
+      <h1>Hello</h1>
+      <Person name={'Prasob'} age={'29'} />
+      <Person name={'Prasob'} age={'29'} >My hobby is codding...!</Person>
     </div>
   );
+
+  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello'))
 }
 
 export default App;
