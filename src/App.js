8e6fd3e1807e0016ac958d1d5d430fda6753396b
Prasob kumar kp <prasobkumarkp@gmail.com>
2020-07-04 23:48:44 +0530

    Revert "react hook"
    This reverts commit d905bd2bab0c8700073e5b5db07105c4dc526744.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
-import React, { useState } from "react";
-import "./App.css";
-import Person from "./Person/Person";
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
-const App = () => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+
+  state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Stephanie", age: 26 },
-      { name: "Manu", age: 29 },
-    ],
-    otherState: "this will get overwritten in useState but not in state",
-  });
+      { name: 'Max', age: 228 },
+      { name: 'Stephanie', age: 26 },
+      { name: 'Manu', age: 29 }
+    ]
+  }
 
-  const [otherState, setOtherState] = useState("other some value");
-  console.log(personsState, otherState);
+  switchNameHandler = () => {
+    this.setState({ persons: [{ name: 'Max', age: 228 }] })
+    // this will merge with the state, not overwriting the state.
+  }
 
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons: [
-        { name: "Maxmillian", age: 29 },
-        { name: "Stephanie", age: 27 },
-        { name: "Manu", age: 30 },
-      ],
-    });
-    // reactHook
-    // this will not merge with the state, and will overwriting the state.
-  };
-
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <button onClick={switchNameHandler}>Switch names</button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age}
-      />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-      />
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age}
-      />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <button onClick={this.switchNameHandler}>Switch names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
   // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello'))
-};
+}
 
 export default App;
