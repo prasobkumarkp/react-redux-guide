b63386c91862a075119f71b195f48c9445936e1e
Prasob kumar kp <prasobkumarkp@gmail.com>
2020-07-05 00:08:17 +0530

    passing method ref between components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
-
   state = {
     persons: [
-      { name: 'Max', age: 228 },
-      { name: 'Stephanie', age: 26 },
-      { name: 'Manu', age: 29 }
-    ]
-  }
+      { name: "Max", age: 228 },
+      { name: "Stephanie", age: 26 },
+      { name: "Manu", age: 29 },
+    ],
+    otherSate: "some other value",
+  };
 
-  switchNameHandler = () => {
-    this.setState({ persons: [{ name: 'Max', age: 228 }] })
+  switchNameHandler = (name) => {
+    this.setState({
+      persons: [
+        { name: name, age: 29 },
+        { name: "Stephanie", age: 27 },
+        { name: "Manu", age: 30 },
+      ],
+    });
     // this will merge with the state, not overwriting the state.
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <button onClick={this.switchNameHandler}>Switch names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        {/* this can efficiency issue */}
+        <button onClick={() => this.switchNameHandler("Prasob")}>
+          Switch names
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Prasob!")}
+        >
+          Hobby : Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const person = (props) => {
-    return <div>
-        <p>I'm {props.name} and i'm {props.age}</p>
-        <p>{props.children}</p>
+  return (
+    <div>
+      <p onClick={props.click}>
+        I'm {props.name} and i'm {props.age}
+      </p>
+      <p>{props.children}</p>
     </div>
-}
+  );
+};
 
 export default person;
